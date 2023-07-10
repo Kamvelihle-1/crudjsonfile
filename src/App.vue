5e29b88bdf7019c95100bb3cf3a72c8b1410864c
SmasherK <ksikalan@gmail.com>
2023-07-10 09:12:42 +0200

    fs

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,20 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
+  <NavbarComp/>
   <router-view/>
+  <FooterComp/>
 </template>
+
+<script>
+import NavbarComp from './components/NavbarComp.vue';
+import FooterComp from './components/FooterComp.vue';
+export default {
+  components:{
+    NavbarComp,
+    FooterComp
+
+  }
+}
+</script>
 
 <style>
 #app {
---
+++ src/components/FooterComp.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        <p>Copyright &copy; {{ cDate }} by Kamvelihle Sikalana</p>
+    </div>
+</template>
+
+<script>
+    export default {
+        data(){
+            return{
+                cDate: new Date().getFullYear()
+            }
+        }
+        
+    }
+</script>
+
+<style scoped>
+
+</style>
